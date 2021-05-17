befddc5372ed03c5471dac42448bf12f17a934c6
Mark Kettenis <kettenis@jive.eu>
2021-05-17 10:29:56 +0200

    Explicitly request C++11 support from the compiler

--- setup.py
+++ setup.py
@@ -26,19 +26,19 @@ sourcefiles3 = ['_getAntInfo.cpp']
 sourcefiles4 = ['_XPCal.cpp']
 
 c_ext1 = Extension("_PolConvert", sources=sourcefiles1,
-                  extra_compile_args=["-Wno-deprecated","-O3"],
+                  extra_compile_args=["-Wno-deprecated","-O3","-std=c++11"],
                   libraries=['cfitsio'],
                   include_dirs=[np.get_include()],
                   extra_link_args=["-Xlinker", "-export-dynamic"])
 
 c_ext3 = Extension("_getAntInfo", sources=sourcefiles3,
-                  extra_compile_args=["-Wno-deprecated","-O3"],
+                  extra_compile_args=["-Wno-deprecated","-O3","-std=c++11"],
                   libraries=['cfitsio'],
                   include_dirs=[np.get_include()],
                   extra_link_args=["-Xlinker", "-export-dynamic"])
 
 c_ext4 = Extension("_XPCal",sources=sourcefiles4,
-                  extra_compile_args=["-Wno-deprecated","-O3"],
+                  extra_compile_args=["-Wno-deprecated","-O3","-std=c++11"],
                   include_dirs=[np.get_include()],
                   extra_link_args=["-Xlinker","-export-dynamic"])
 
@@ -51,7 +51,7 @@ if DO_SOLVE:
   c_ext2 = Extension("_PolGainSolve", sources=sourcefiles2,
                   libraries=['gsl','fftw3'],
                   include_dirs=[np.get_include()],
-                  extra_compile_args=["-Wno-deprecated","-O3"],
+                  extra_compile_args=["-Wno-deprecated","-O3","-std=c++11"],
                   extra_link_args=["-Xlinker", "-export-dynamic"])
 
 cfitsio='/usr/include/cfitsio'
