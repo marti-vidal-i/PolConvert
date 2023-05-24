0aacc4345d2fa4c81c41ddb4f7b82d71812da283
gcrew <gbc@haystack.mit.edu>
2023-05-24 11:09:31 -0400

    options for older versions of fringes

--- PP/checkfringedata.py
+++ PP/checkfringedata.py
@@ -18,20 +18,36 @@ import re
 import struct as stk
 import sys
 
-def examineFRINGE_IF(pli, o):
+def dtype0(fringedata,frfile):
     '''
-    pli is the index of the file, so .../POLCONVERT.FRINGE_IF??
-    is expected to hold some binary data this task will try to
-    unpack it and report on what it holds.  Options in o affect
-    what it does with the data.
+    The version with PANG? but not UVDIST..DiFX 2.6 through 2.8.1
+    (through PolConvert version 2.0.3).
     '''
-    fringedata = "%s/POLCONVERT.FRINGE/POLCONVERT.FRINGE_IF%i" % (
-        o.dir,pli)
-    print('reading',os.path.basename(fringedata),'...')
-    frfile = open(fringedata,"rb")
+    print('reading',os.path.basename(fringedata),'...',end=' ')
+    alldats = frfile.read(4)
+    nchPlot = stk.unpack("i?", alldats)
+    print('no UVDIST')
+    dtype = np.dtype(
+        [
+            ("FILE", np.int32),
+            ("JDT", np.float64),
+            ("ANT1", np.int32),
+            ("ANT2", np.int32),
+            ("PANG1", np.float64),
+            ("PANG2", np.float64),
+            ("MATRICES", np.complex64, 12 * nchPlot),
+        ]
+    )
+    return dtype,nchPlot
+
+def dtype1(fringedata,frfile):
+    '''
+    The version with PANG? and UVDIST..DiFX 2.8.2 (after 2.0.4)
+    '''
+    print('reading',os.path.basename(fringedata),'...',end=' ')
     alldats = frfile.read(5)
     nchPlot,isParang = stk.unpack("i?", alldats)
-    print(' ',nchPlot,'channels, with Parang?',isParang)
+    print('with Parang?',isParang,'w/UVDIST')
     dtype = np.dtype(
         [
             ("FILE", np.int32),
@@ -44,13 +60,28 @@ def examineFRINGE_IF(pli, o):
             ("MATRICES", np.complex64, 12 * nchPlot),
         ]
     )
+    return dtype,nchPlot
+
+def examineFRINGE_IF(pli, o):
+    '''
+    pli is the index of the file, so .../POLCONVERT.FRINGE_IF??
+    is expected to hold some binary data this task will try to
+    unpack it and report on what it holds.  Options in o affect
+    what it does with the data.
+    '''
+    fringedata = "%s/POLCONVERT.FRINGE/POLCONVERT.FRINGE_IF%i" % (
+        o.dir,pli)
+    frfile = open(fringedata,"rb")
+    if o.pcvers == '0': dtype,nchPlot = dtype0(fringedata,frfile)
+    elif o.pcvers == '1': dtype,nchPlot = dtype1(fringedata,frfile)
+    else: raise Exception('Unsupported fringe version ' + o.pcvers)
     try:
         fringe = np.fromfile(frfile,dtype=dtype)
     except Exception as ex:
         print('Unable to read fringe',str(ex))
     frfile.close()
     print(' ',os.path.basename(fringedata),
-        'successfully with',len(fringe),'parts:')
+        'successfully with',len(fringe),'blocks and',nchPlot,'channels')
     x = len(fringe)-1
     print('  [%04d] File:'%0,fringe[0]['FILE'],
         'JDT %f s = %s'%jdt(fringe[0]['JDT']))
@@ -58,12 +89,14 @@ def examineFRINGE_IF(pli, o):
         'JDT %f s = %s'%jdt(fringe[x]['JDT']))
     print('  ANT1: ',set(list(fringe[:]["ANT1"])),
         ', ANT2: ',set(list(fringe[:]["ANT2"])))
+    maxUVDIST = ''
+    if o.pcvers == '1': maxUVDIST = (
+        ' max UVDIST %f'%np.max(fringe[:]["UVDIST"]) + '(units unknown)')
     print('  PANG1: %.2f'%np.rad2deg(np.min(fringe[:]["PANG1"])),
         '.. %.2f'%np.rad2deg(np.max(fringe[:]["PANG1"])),
         '  PANG2: %.2f'%np.rad2deg(np.min(fringe[:]["PANG2"])),
         '.. %.2f'%np.rad2deg(np.max(fringe[:]["PANG2"])),
-        ' (deg);\n',
-        ' max UVDIST %f'%np.max(fringe[:]["UVDIST"]),'(units unknown)')
+        ' (deg);\n', maxUVDIST)
 
 def jdt(jdts):
     '''
@@ -141,7 +174,29 @@ def parseOptions():
         default=20, metavar=int, help='Threshold for numpy printing')
     parser.add_argument('-w', '--linewidth', dest='width', type=int,
         default=70, metavar=int, help='Linewidth for numpy printing')
+    parser.add_argument('-V', '--pcvers', dest='pcvers',
+        default='1', help='Fringe file version: 1 = 2.0.5 and later'
+        ' (with UVDIST), 0 = 2.0.3 and earlier without it, or help')
+    # FIXME: plot options
     return parser.parse_args()
+
+def somehelp(o):
+    pcvershelp='''
+    The fringe file is binary packed for numpy to read it
+    The early versions had parallactic angles (not implemented)
+    and as of 2.0.5 (targetted for DiFX 2.8.2), UVDIST was added.
+    Use -V 0 for the earlier format and -V 1 for the later one.
+    '''
+    plothelp='''
+
+    FIXME: The next shoe is options to generate plots....
+
+    '''
+    if o.pcvers == 'help':
+        print(pcvershelp)
+        return True
+    #if o.wpcvershat == 'plots': return plothelp
+    return False
 
 #
 # enter here to do the work
@@ -151,6 +206,7 @@ if __name__ == '__main__':
         print('Sorry, this code is Python3 only')
         sys.exit(1)
     o = parseOptions()
+    if somehelp(o): sys.exit(0)
     if o.verb: print('printing with %d precision, %d elements, %d width' % (
         o.prec, o.thres, o.width))
     np.set_printoptions(
