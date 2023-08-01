34fae4ece9f2dba1e93d971782876f9795aeea47
gcrew <gbc@haystack.mit.edu>
2023-08-01 10:02:43 -0400

    Version bump to 2.0.6 prior to testing

--- TOP/task_polconvert.py
+++ TOP/task_polconvert.py
@@ -50,8 +50,8 @@
 
 from __future__ import absolute_import
 from __future__ import print_function
-__version__ = "2.0.5"
-date = 'May 23, 2023'
+__version__ = "2.0.6"
+date = 'Aug 1, 2023'
 
 
 ################
--- polconvert_CASA.py
+++ polconvert_CASA.py
@@ -50,8 +50,8 @@
 
 from __future__ import absolute_import
 from __future__ import print_function
-__version__ = "2.0.5  "  # 7 characters
-date = 'May 23, 2023'
+__version__ = "2.0.6  "  # 7 characters
+date = 'Aug 1, 2023'
 
 
 ################
